1f482cfccbfb10be74f7f32e2c994f62ffb8887e
getromandev <getroman.dev@gmail.com>
2020-10-22 14:10:22 -0400

    connect to MongoDB and Mongoose

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose'); 
+
+const { MONGODB } = require('./config.js');
 
 const typeDefs = gql `
     type Query {
@@ -18,7 +21,13 @@ const server = new ApolloServer({
     resolvers
 });
 
-server.listen({ port: 5000})
+// connect to mongoose 
+mongoose
+    .connect(MONGODB, { useNewUrlParser: true})
+    .then(() => {
+        console.log('MONGODB Connected');
+        return server.listen({ port: 5000});
+    })
     .then(res => {
        console.log(`Server running at ${res.url}`) 
     })
