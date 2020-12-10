d5ac28ceb86c07d24989aad2256aa10ad217efda
getromandev <getroman.dev@gmail.com>
2020-12-09 19:35:35 -0500

    Set up semantic UI & routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import MenuBar from './components/MenuBar'
+import { Container } from 'semantic-ui-react';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to my social media site</h1>
-    </div>
-  );
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Container>
+    </Router>
+  )
 }
 
 export default App;
