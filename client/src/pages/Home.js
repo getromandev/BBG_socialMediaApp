90e5527948417d049c98e48be4ed2f24e744c1f3
getromandev <getroman.dev@gmail.com>
2020-12-12 23:17:33 -0500

    ft-displaying-posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,63 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { Grid } from 'semantic-ui-react';
+
+import PostCard from '../components/PostCard';
+// Query the DB 
+const FETCH_POSTS_QUERY = gql `
+    {
+        getPosts{
+            id 
+            body 
+            createdAt 
+            username 
+            likeCount
+            likes {
+                username
+            }
+            commentCount
+            comments {
+                id 
+                username 
+                createdAt 
+                body
+            }
+        }
+    }
+`
 
 function Home() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+    const { loading, data, error } = useQuery(FETCH_POSTS_QUERY);
+
+    if (loading) return 'Loading mother fucker...'
+    if (error) return `Error ${error.message}`;
+
+    // test for data coming into the Component
+    if(data) {
+        console.log(data);
+        console.log('data.getPosts >>', data.getPosts);
+    }
+
+     return (
+        <Grid columns={3} divided>
+            <Grid.Row className='page-title'>
+                <h1>Recent Posts</h1>
+            </Grid.Row>
+            <Grid.Row>
+                {loading ? (
+                    <h1>Loading Post..</h1>
+                ) : ( 
+                    data.getPosts.map(post => (
+                        <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                            <PostCard post={post} />
+                        </Grid.Column>
+                    ))
+                )}
+            </Grid.Row>
+        </Grid>
     )
 }
+
 
 export default Home;
